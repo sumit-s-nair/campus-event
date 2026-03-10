d80668c485dc16cad1abe1f0fc0099587605d6e0
Sumit Santhosh Nair <sumitnair731@gmail.com>
2026-03-10 16:16:56 +0000

    chore: initialize spring boot application

---
+++ src/main/java/com/example/campuseventplatform/CampusEventPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.campuseventplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusEventPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusEventPlatformApplication.class, args);
+    }
+
+}
